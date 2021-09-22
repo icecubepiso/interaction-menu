e0d1b45aaba2479155630accf93c95b4a7972ae2
IceCubePiso <michalsnasel448@gmail.com>
2021-09-22 18:02:08 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -13,3 +13,22 @@ function HideInteraction()
 	})
 end
 exports('HideInteraction', HideInteraction)
+
+
+--- idk if i need it.. but what ever xD
+
+RegisterNetEvent('interaction-menu:ShowInteraction')
+AddEventHandler('interaction-menu:ShowInteraction', function(action, color, text)
+	SendNUIMessage({
+		action = action,
+		color = color, 
+		text = text,
+	})
+end)
+
+RegisterNetEvent('interaction-menu:HideInteraction')
+AddEventHandler('interaction-menu:HideInteraction', function()
+	SendNUIMessage({
+		action = 'hide'
+	})
+end)
